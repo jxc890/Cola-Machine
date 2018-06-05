78d3cd847df6ccab7032893b74d9550a8e11945b
Jiakai Chen <jchen154@ucsc.edu>
2018-06-05 21:33:37 +0800

    FullVersion

--- Cola Machine/main.cpp
+++ Cola Machine/main.cpp
@@ -7,9 +7,34 @@
 //
 
 #include <iostream>
+using namespace std;
 
 int main(int argc, const char * argv[]) {
     // insert code here...
-    std::cout << "Hello, World!\n";
+    cout << "Enter Number Choosing your Drink!" << endl;
+    cout << "1.Cococola"<< "   " << "2.Pepsi"<< "   " << "3.CanadaDry"<< "   " << "4.SevenUp"<< "   " << "5.Sprite" << endl;
+    int number;
+    cin >> number;
+    switch (number) {
+        case 1:
+            cout << "Cococola Out!" <<endl;
+            break;
+        case 2:
+            cout << "Pepsi Out!" <<endl;
+            break;
+        case 3:
+            cout << "CanadaDry Out!" <<endl;
+            break;
+        case 4:
+            cout << "SevenUp Out!" <<endl;
+            break;
+        case 5:
+            cout << "Sprite Out!" <<endl;
+            break;
+            
+        default:
+            cout << "Error. choice was not valid, here is your money back." <<endl;
+            break;
+    }
     return 0;
 }
